6a540720c31f9cc741ae43f3677f422839b71ed6
Romain <romain.rousseau5@gmail.com>
2020-10-26 12:21:01 +0100

    editing home page model and moving it to pages folding

---
+++ sanity/schemas/pages/home.js
@@ -0,0 +1,128 @@
+export default {
+  name: "home",
+  title: "Home page",
+  type: "document",
+  fieldsets: [
+    { name: "heroSection", title: "Hero section" },
+    { name: "communitySection", title: "Community section" },
+    { name: "partnersSection", title: "Partners section" },
+    { name: "platformSection", title: "Platform section" },
+    { name: "ctaSection", title: "CTA section" },
+  ],
+  fields: [
+    // hero section
+    {
+      name: "planetImage",
+      title: "Planet image",
+      type: "image",
+      fieldset: "heroSection",
+    },
+    {
+      name: "heroTitle",
+      title: "Title",
+      type: "string",
+      fieldset: "heroSection",
+    },
+    {
+      title: "Video file",
+      name: "video",
+      type: "file",
+      fieldset: "heroSection",
+    },
+    {
+      name: "heroDescription",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+      fieldset: "heroSection",
+    },
+    // partners section
+    {
+      name: "partnersTitle",
+      title: "Title",
+      type: "string",
+      fieldset: "partnersSection",
+    },
+    {
+      name: "partners",
+      title: "Partners",
+      type: "array",
+      of: [
+        {
+          type: "reference",
+          to: [{ type: "partner" }],
+        },
+      ],
+      fieldset: "partnersSection",
+    },
+    // community section
+    {
+      name: "communityTitle",
+      title: "Title",
+      type: "string",
+      fieldset: "communitySection",
+    },
+    {
+      name: "communityImages",
+      title: "Images",
+      type: "array",
+      of: [{ type: "image" }],
+      fieldset: "communitySection",
+    },
+    {
+      name: "communityDescription",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+      fieldset: "communitySection",
+    },
+
+    // platform section
+    {
+      name: "platformTitle",
+      title: "Title",
+      type: "string",
+      fieldset: "platformSection",
+    },
+    {
+      name: "platformDescription",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+      fieldset: "platformSection",
+    },
+    {
+      name: "platformImage",
+      title: "Platform Image",
+      type: "image",
+      fieldset: "platformSection",
+    },
+
+    // cta section
+    {
+      name: "ctaTitle",
+      title: "Title",
+      type: "string",
+      fieldset: "ctaSection",
+    },
+    {
+      name: "ctaDescription",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+      fieldset: "ctaSection",
+    },
+    {
+      name: "ctas",
+      title: "Call to actions",
+      type: "array",
+      of: [
+        {
+          type: "reference",
+          to: [{ type: "cta" }],
+        },
+      ],
+      fieldset: "ctaSection",
+    },
+  ],
+};
