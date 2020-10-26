7b459d0425876feb2a131296a411f334353b98ba
Romain <romain.rousseau5@gmail.com>
2020-10-26 12:21:31 +0100

    adding new models to sanity desk

--- sanity/deskStructure.js
+++ sanity/deskStructure.js
@@ -2,29 +2,31 @@ import S from "@sanity/desk-tool/structure-builder";
 
 // icons
 import pagesIcon from "react-icons/lib/md/book";
+import partnerIcon from "react-icons/lib/fa/hand-grab-o";
 
 export default () =>
-    S.list()
-        .title("Welcome, Space Farmer!")
-        .items([
-            // First pane
-            S.listItem()
-                .title("Pages")
-                .icon(pagesIcon)
+  S.list()
+    .title("Welcome, Space Farmer!")
+    .items([
+      // First pane
+      S.listItem()
+        .title("Pages")
+        .icon(pagesIcon)
+        .child(
+          // Second pane
+          S.list()
+            .title("All pages")
+            .items([
+              // Landing page
+              S.listItem()
+                .title("Home")
                 .child(
-                    // Second pane
-                    S.list()
-                        .title("All pages")
-                        .items([
-                            // Landing page
-                            S.listItem()
-                                .title("Home")
-                                .child(
-                                    S.editor()
-                                        .schemaType("home")
-                                        .documentId("home")
-                                        .title("Edit Homepage")
-                                ),
-                        ])
+                  S.editor()
+                    .schemaType("home")
+                    .documentId("home")
+                    .title("Edit Homepage")
                 ),
-        ]);
+            ])
+        ),
+      ...S.documentTypeListItems(),
+    ]);
