d225d62b76734e87a562b0f09d3407649362bb23
Romain <romain.rousseau5@gmail.com>
2020-11-03 15:28:54 +0100

    adding links and animation to social icons

--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -5,18 +5,37 @@ import Instagram from "../public/icons/instagram.svg";
 import Twitter from "../public/icons/twitter.svg";
 import styles from "./modules/SocialIcons.module.css";
 
+const ExternalLink = ({ className, href, children }) => {
+  return (
+    <a
+      target="_blank"
+      href={href}
+      referrerPolicy="no-referrer"
+      className={className}
+    >
+      {children}
+    </a>
+  );
+};
+
 export default function SocialIcons({ color, className }) {
   return (
     <div className={`${styles.container} ${styles[color]} ${className}`}>
-      <a className={`${styles.icon} ${styles[color]}`}>
+      <ExternalLink
+        href={"https://www.instagram.com/astro_plant/"}
+        className={`${styles.icon} ${styles[color]}`}
+      >
         <Instagram />
-      </a>
-      <a className={`${styles.icon} ${styles[color]}`}>
+      </ExternalLink>
+      <ExternalLink className={`${styles.icon} ${styles[color]}`}>
         <Facebook />
-      </a>
-      <a className={`${styles.icon} ${styles[color]}`}>
+      </ExternalLink>
+      <ExternalLink
+        href={"https://twitter.com/astroplant"}
+        className={`${styles.icon} ${styles[color]}`}
+      >
         <Twitter />
-      </a>
+      </ExternalLink>
     </div>
   );
 }
