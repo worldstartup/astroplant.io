f1a46c48433d0acbe8ec9d542939c534eeaa41da
Romain <romain.rousseau5@gmail.com>
2020-11-04 12:18:47 +0100

    fixing team members section layout and adding linkedin icon hover animation

--- components/sections/TeamSection.js
+++ components/sections/TeamSection.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 import CoreTeamMember from "../CoreTeamMember";
+import ContentLayout from "../layouts/ContentLayout";
 import styles from "../modules/TeamSection.module.css";
 import Section from "../Section";
 import TeamMember from "../TeamMember";
@@ -8,34 +9,36 @@ import TeamMember from "../TeamMember";
 export default function TeamSection({ coreTeamMembers, otherTeamMembers }) {
   return (
     <Section id="team" bgColor={"dark-light"}>
-      <h2>The Core Team</h2>
-      <div className={`${styles.grid} ${styles.coreMembersGrid}`}>
-        {coreTeamMembers.map((member) => (
-          <CoreTeamMember key={member._id} teamMember={member} />
-        ))}
-      </div>
+      <ContentLayout>
+        <h2 className={styles.sectionSubTitle}>The Core Team</h2>
+        <div className={`${styles.grid} ${styles.coreMembersGrid}`}>
+          {coreTeamMembers.map((member) => (
+            <CoreTeamMember key={member._id} teamMember={member} />
+          ))}
+        </div>
 
-      <h2 className={styles.sectionSubTitle}>Our Advisors</h2>
+        <h2 className={styles.sectionSubTitle}>Our Advisors</h2>
 
-      <div className={`${styles.grid} ${styles.otherMembersGrid}`}>
-        {otherTeamMembers.map(
-          (member) =>
-            member.memberOf === "Advisors" && (
-              <TeamMember key={member._id} teamMember={member} />
-            )
-        )}
-      </div>
+        <div className={`${styles.grid} ${styles.otherMembersGrid}`}>
+          {otherTeamMembers.map(
+            (member) =>
+              member.memberOf === "Advisors" && (
+                <TeamMember key={member._id} teamMember={member} />
+              )
+          )}
+        </div>
 
-      <h2 className={styles.sectionSubTitle}>Former Members</h2>
+        <h2 className={styles.sectionSubTitle}>Former Members</h2>
 
-      <div className={`${styles.grid} ${styles.otherMembersGrid}`}>
-        {otherTeamMembers.map(
-          (member) =>
-            member.memberOf === "Former Team" && (
-              <TeamMember key={member._id} teamMember={member} />
-            )
-        )}
-      </div>
+        <div className={`${styles.grid} ${styles.otherMembersGrid}`}>
+          {otherTeamMembers.map(
+            (member) =>
+              member.memberOf === "Former Team" && (
+                <TeamMember key={member._id} teamMember={member} />
+              )
+          )}
+        </div>
+      </ContentLayout>
     </Section>
   );
 }
