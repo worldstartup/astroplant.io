660d7fee39bbfad03d441103eb2c27a26013af62
Romain Rousseau <romain.rousseau5@gmail.com>
2020-11-05 16:18:55 +0100

    adding loading animation to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import Carousel from "../components/Carousel";
 import MainLayout from "../components/layouts/MainLayout";
+import Loader from "../components/Loader";
 import Section from "../components/Section";
 import CommunitySection from "../components/sections/CommunitySection";
 import CTASection from "../components/sections/CTASection";
@@ -9,63 +11,63 @@ import PlatformSection from "../components/sections/PlatformSection";
 import { getFeaturedArticles } from "../services/community-api";
 import { getAchievements, getHome } from "../services/sanity";
 
-/*const Loader = dynamic(
-  () => import("react-preloaders").then((loaders) => loaders.Cube),
-  {
-    ssr: false,
-  }
-);*/
-
 export default function Home({ data, achievements, featuredArticles }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(function () {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [loading]);
+
   return (
-    <div className={"bg-gradient"}>
-      <MainLayout
-        pageName={"Home"}
-        pageDescription={
-          "AstroPlant, growing a new generation of urban and space farmers."
-        }
-        featuredArticles={featuredArticles}
-      >
-        {/*<Loader
-        color={"var(--main-color)"}
-        time={1000}
-        animation="slide"
-        background="linear-gradient(321deg, rgba(28,19,31,1) 0%, rgba(20,15,43,1) 100%)"
-      />*/}
-        <HeroSection
-          title={data.heroTitle}
-          description={data.heroDescription}
-          image={data.planetImage}
-          videoUrl={data.video}
-        />
-        <MainPartnersSection
-          title={data.partnersTitle}
-          partners={data.partners}
-          link={data.partnersLink}
-        />
-        <CommunitySection
-          title={data.communityTitle}
-          description={data.communityDescription}
-          images={data.communityImages}
-          link={data.communityLink}
-        />
-        <Section id="achievements">
-          <Carousel content={achievements} />
-        </Section>
-        <PlatformSection
-          title={data.platformTitle}
-          description={data.platformDescription}
-          image={data.platformImage}
-          links={data.platformLinks}
-        />
-        <CTASection
-          title={data.ctaTitle}
-          description={data.ctaDescription}
-          image={data.ctaImage}
-          ctas={data.ctas}
-        />
-      </MainLayout>
-    </div>
+    <>
+      <Loader show={loading} />
+      <div className={`bg-gradient ${loading ? "hide-content" : ""}`}>
+        <MainLayout
+          pageName={"Home"}
+          pageDescription={
+            "AstroPlant, growing a new generation of urban and space farmers."
+          }
+          featuredArticles={featuredArticles}
+        >
+          <HeroSection
+            title={data.heroTitle}
+            description={data.heroDescription}
+            image={data.planetImage}
+            videoUrl={data.video}
+          />
+          <MainPartnersSection
+            title={data.partnersTitle}
+            partners={data.partners}
+            link={data.partnersLink}
+          />
+          <CommunitySection
+            title={data.communityTitle}
+            description={data.communityDescription}
+            images={data.communityImages}
+            link={data.communityLink}
+          />
+          <Section id="achievements">
+            <Carousel content={achievements} />
+          </Section>
+          <PlatformSection
+            title={data.platformTitle}
+            description={data.platformDescription}
+            image={data.platformImage}
+            links={data.platformLinks}
+          />
+          <CTASection
+            title={data.ctaTitle}
+            description={data.ctaDescription}
+            image={data.ctaImage}
+            ctas={data.ctas}
+          />
+        </MainLayout>
+      </div>
+    </>
   );
 }
 
